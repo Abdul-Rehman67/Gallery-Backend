6c095a4ccbb4fb4e93cdd6c63bae7216cf352e44
Abdul Rehman Shaikh <arehmanshaikh@folio3.com>
2024-05-20 17:24:20 +0500

    IMAGE TYPE ONLY

--- src/util/multerConfig.js
+++ src/util/multerConfig.js
@@ -14,8 +14,23 @@ const storage = multer.diskStorage({
     cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
+
+const imageFileFilter = (req, file, cb) => {
+  const fileTypes = /jpeg|jpg|png|gif/;
+  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = fileTypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only!');
+  }
+};
 console.log("storage",storage)
 
-const upload = multer({ storage });
-
+const upload = multer({
+  storage: storage,
+  fileFilter: imageFileFilter,
+  limits: { fileSize: 1024 * 1024 * 5 }, 
+})
 module.exports = upload;
